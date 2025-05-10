d80576337a632bb79a2aba7025aae83d7a279aad
Nuraddin Abbasov <nuraddinabbas@gmail.com>
2025-05-10 13:16:29 +0300

    isPrime function fixed

--- algorithm_project1.c
+++ algorithm_project1.c
@@ -140,7 +140,7 @@ typedef struct {
 int isPrime(int n) {
     if (n <= 1) return 0;
 	int i;
-    for (i = 2; i <= n*n; i++)
+    for (i = 2; i*i <= n; i++)
         if (n % i == 0) return 0;
     return 1;
 }
